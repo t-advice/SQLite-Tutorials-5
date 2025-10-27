733e710040e0ebab31359cf8a32d311be2f6349a
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-27 19:03:10 +0200

    code doen for mainpage logic

--- SQLiteTutorial5/Views/MainPage.xaml.cs
+++ SQLiteTutorial5/Views/MainPage.xaml.cs
@@ -1,14 +1,50 @@
-﻿namespace SQLiteTutorial5
+﻿using SQLiteTutorial5.Models;
+using System.Collections.ObjectModel;
+
+namespace SQLiteTutorial5.Views
 {
     public partial class MainPage : ContentPage
     {
-        //int count = 0;
+        public ObservableCollection<Book> Books { get; set; } = new();
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-       
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadBooks();
+        }
+
+        private async Task LoadBooks()
+        {
+            Books.Clear();
+            var list = await App.Database.GetBooksAsync();
+            foreach (var book in list)
+                Books.Add(book);
+
+            BooksCollection.ItemsSource = Books;
+        }
+
+        private async void OnAddBookClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(AddBookPage));
+        }
+
+        private async void OnDeleteBookClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is Book book)
+            {
+                bool confirm = await DisplayAlert("Delete", $"Delete '{book.Title}'?", "Yes", "No");
+                if (confirm)
+                {
+                    await App.Database.DeleteBookAsync(book);
+                    await LoadBooks();
+                }
+            }
+        }
     }
 }
+
