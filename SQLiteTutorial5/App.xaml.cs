183c9d5063717e513a2e7df721a49675c80b76d6
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-27 17:32:23 +0200

    database coded and created globally , including path creation on device.

--- SQLiteTutorial5/App.xaml.cs
+++ SQLiteTutorial5/App.xaml.cs
@@ -1,15 +1,36 @@
-﻿namespace SQLiteTutorial5
+﻿using SQLiteTutorial5.Services;
+
+namespace SQLiteTutorial5
 {
     public partial class App : Application
     {
+        private static DatabaseService _database;
+
+        public static DatabaseService Database
+        {
+            get
+            {
+                if(_database == null)
+                {
+                    string dbPath = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "Books.db3");
+
+                    _database = new DatabaseService(dbPath);
+                }
+                return _database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
+            MainPage = new AppShell();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //}
     }
 }
