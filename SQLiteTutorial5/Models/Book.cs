f412824adf9919b81a5e5c9bf6d60cb80895b311
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-27 14:45:19 +0200

    Model Object class created called book

---
+++ SQLiteTutorial5/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Renderscripts;
+using SQLite;
+
+namespace SQLiteTutorial5.Models
+{
+    public class Book
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(150)]
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
